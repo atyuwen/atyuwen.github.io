556c4f4f909531a26874154b235097bb8fd95cc0
atyuwen <atyuwen@gmail.com>
2026-03-25 21:20:43 +0800

    Deploy: Add Transformer article and update uarch

---
+++ transformer/sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'transformer-v1';
+var URLS_TO_CACHE = ['./', './index.html'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
